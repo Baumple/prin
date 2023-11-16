658b77897fe6109772db5fc668e9f3bf8d84de93
Baumple <thziegler52@gmail.com>
2023-11-16 14:32:19 +0100

    Yes

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,14 @@ import anagram.Anagram;
 import calendar.Appointment;
 import calendar.AppointmentCalendar;
 import calendar.Date;
+import circle.Circle;
 
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        var calendar = new AppointmentCalendar();
-        var appointments = List.of(
-                new Appointment("03.12.2023", "test"),
-                new Appointment("03.02.1999", "appointment"),
-                new Appointment("09.12.2003", "termin"),
-                new Appointment("31.3.2", "test")
-        );
-        appointments.forEach(calendar::addAppointment);
-        calendar.showAllAppointments();
 
-        calendar.isAppointmentAtDate(new Date("03.12.2023"));
-
+        Circle.calculate();
     }
 }
---
+++ src/circle/Circle.java
@@ -0,0 +1,24 @@
+package circle;
+
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Circle {
+    public static void calculate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter radius: ");
+
+        double radius = scanner.nextDouble();
+        if (radius <= 0) {
+            System.out.println("Radius has to be larger than 0!");
+            return;
+        }
+
+        System.out.println("Area is: " + area(radius));
+
+    }
+
+    public static double area(double r) {
+        return Math.PI * r * r;
+    }
+}
